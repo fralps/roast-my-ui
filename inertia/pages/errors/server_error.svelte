7b67ad6e77e43a741690897f0a4c040a6a2813c5
fralps <francois@kinoba.fr>
2024-05-29 14:42:45 +0200

    chore: first commit

---
+++ inertia/pages/errors/server_error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let error: { message: string }
+</script>
+
+<div>
+  <div class="container">
+    <div class="title">Server Error</div>
+
+    <span>{error.message}</span>
+  </div>
+</div>
---
+++ inertia/pages/home.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Homepage</title>
+</svelte:head>
+
+<div class="container">
+  <div class="title">RoastMyUI</div>
+</div>
