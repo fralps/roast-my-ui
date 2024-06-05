dbc87e1d624a7931e3850802b3f445eca7c93975
fralps <francois@kinoba.fr>
2024-06-05 20:44:30 +0200

    chore: add tailwindcss

--- inertia/pages/home.svelte
+++ inertia/pages/home.svelte
@@ -3,5 +3,5 @@
 </svelte:head>
 
 <div class="container">
-  <div class="title">RoastMyUI</div>
+  <div class="text-red-500 text-4xl">RoastMyUI</div>
 </div>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/**/*.edge',
+    './resources/**/*.{js,ts,jsx,tsx,svelte}',
+    './inertia/**/*.{js,ts,jsx,tsx,svelte}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
