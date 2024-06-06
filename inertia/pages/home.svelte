ffcabc285746681c450ba18c31f09c2ab81f863d
fralps <francois@kinoba.fr>
2024-06-06 16:02:59 +0200

    feat: remove font animation

--- inertia/pages/home.svelte
+++ inertia/pages/home.svelte
@@ -11,7 +11,7 @@
 <div>
   <h1 class="text-6xl text-center nabla">
     {#each title as letter, index}
-      <span class="animated-title" style="animation-delay: {0.0 + index * 0.1}s">{letter}</span>
+      <span style="animation-delay: {0.0 + index * 0.1}s">{letter}</span>
     {/each}
   </h1>
   <div class="mt-10 text-center"><Link href="/login">Login</Link></div>
