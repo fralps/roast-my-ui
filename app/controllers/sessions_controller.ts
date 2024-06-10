7badc885a067cd96b1e2dfb031b26ab10cd504fa
fralps <francois@kinoba.fr>
2024-06-10 17:26:48 +0200

    feat: remove useless logs

--- app/controllers/sessions_controller.ts
+++ app/controllers/sessions_controller.ts
@@ -13,22 +13,17 @@ export default class SessionsController {
     const roaster = await Roaster.findBy('email', email)
     const roastee = await Roastee.findBy('email', email)
 
-    console.log('ROASTER', roaster)
-    console.log('ROASTEE', roastee)
-
     if (!roaster && !roastee) {
       return response.redirect().toRoute('sessions.create')
     }
 
     if (roaster) {
       const roasterAuth = await Roaster.verifyCredentials(email, password)
-      console.log('IF Roaster')
       await auth.use('roaster').login(roasterAuth)
       return response.redirect().toRoute('proposals.index')
     }
 
     if (roastee) {
-      console.log('IF Roastee')
       const roasteeAuth = await Roastee.verifyCredentials(email, password)
       await auth.use('roastee').login(roasteeAuth)
       return response.redirect().toRoute('roastee_proposals.index')
