712ecb013179558f9bae6177a35bd057ba4dc993
fralps <francois@kinoba.fr>
2024-06-12 09:43:52 +0200

    feat: add proposal creation component by roastee

--- inertia/pages/roastee/proposals/create.svelte
+++ inertia/pages/roastee/proposals/create.svelte
@@ -1,1 +1,12 @@
-Hello
+<script lang="ts">
+  import { Navbar } from '@components'
+
+  const title = 'New proposal'
+</script>
+
+<svelte:head>
+  <title>RoastMyUI - {title}</title>
+</svelte:head>
+
+<Navbar />
+<section class="pb-10 mt-10">Hello</section>
